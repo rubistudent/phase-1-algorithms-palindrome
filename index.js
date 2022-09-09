2ce6d4cfff46955cd72f8ba63fa4fabae7dc0432
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2022-09-09 21:14:33 +0300

    complited assinments

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
-// reverse the input string
-const reversedWord = reverseString(word);
-// compare the reversed string to the input
-return word === reversedWord;
+function isPalindrome(word) {
+	// reverse the input string
+	const reversedWord = reverseString(word);
+	// compare the reversed string to the input
+	return word === reversedWord;
 }
 
 function reverseString(word) {
-return word.split("").reverse().join("");
+	return word.split("").reverse().join("");
 }
 
 /* 
-Add your pseudocode here
-reverse the input string
+  Add your pseudocode here
+  reverse the input string
 if the reversed string is the same as the input
-return true
+  return true
 else
-return false
+  return false
 */
 
 /*
-Add written explanation of your solution here
-We are going to use the reverseString function to reverse the input string. Then we will compare the reversed string to the input string. If they are the same, we will return true. If they are not the same, we will return false.
-
+  Add written explanation of your solution here
+  We are going to use the reverseString function to reverse the input string. Then we will compare the reversed string to the input string. If they are the same, we will return true. If they are not the same, we will return false.
+  
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-// add your own custom tests in here
-console.log("Expecting: true");
-console.log("=>", isPalindrome("racecar"));
+	// add your own custom tests in here
+	console.log("Expecting: true");
+	console.log("=>", isPalindrome("racecar"));
 
-console.log("");
+	console.log("");
 
-console.log("Expecting: false");
-console.log("=>", isPalindrome("robot"));
+	console.log("Expecting: false");
+	console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
